fede6dde0841b8d2a3ce55102f633e62c92ccf8b
Yenny <yenny.waitoto.mt@correo.usa.edu.co>
2021-11-14 19:58:20 -0500

    Ajuste de web Skate

--- src/main/java/com/usa/ciclo3/reto3/web/SkateControllerWeb.java
+++ src/main/java/com/usa/ciclo3/reto3/web/SkateControllerWeb.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/Partyroom")
+@RequestMapping("/api/Skate")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 
 public class SkateControllerWeb {
